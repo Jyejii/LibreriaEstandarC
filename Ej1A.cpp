b3fc3694b86df05029e2c8ff03ec1c13cb368515
Jiachen <146822199+Jyejii@users.noreply.github.com>
2023-12-19 01:35:36 +0100

    Funciones Comunes

---
+++ Ej1A.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Ingresa un número entero: ");
+    scanf("%d", &num);
+    
+    int abs_num = abs(num);
+
+    printf("El valor absoluto de %d es %d\n", num, abs_num);
+
+    return 0;
+}
