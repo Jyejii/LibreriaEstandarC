9bf02c4600262e0627ec43d9b85265725f1fcca7
Jiachen <jye@myuax.com>
2023-12-19 01:50:06 +0100

    Ej2

--- Ej2.c
+++ Ej2.c
@@ -1,3 +1,21 @@
-//
-// Created by jiach on 19/12/2023.
-//
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    const char *str1 = "Hola";
+    const char *str2 = "Mundo";
+
+    printf("Comparando las cadenas:\n");
+    printf("Cadena 1: %s\n", str1);
+    printf("Cadena 2: %s\n", str2);
+
+    int comparison = strcmp(str1, str2);
+
+    if (comparison == 0) {
+        printf("Las cadenas son iguales.\n");
+    } else {
+        printf("Las cadenas son diferentes.\n");
+    }
+
+    return 0;
+}
