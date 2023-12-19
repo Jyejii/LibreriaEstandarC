e4b277284f66ab194658398bdd90f33bbe563e34
Jiachen <jye@myuax.com>
2023-12-19 02:20:44 +0100

    ejercicio CompareStrings.c dado por el enunciado

--- CompareStrings.c
+++ CompareStrings.c
@@ -1,3 +1,17 @@
-//
-// Created by jiach on 19/12/2023.
-//
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    char str1[] = "Hola";
+    char str2[] = "Mundo";
+
+    int comparison = strcmp(str1, str2);
+
+    if (comparison == 0) {
+        printf("Las cadenas son iguales.\n");
+    } else {
+        printf("Las cadenas son diferentes.\n");
+    }
+
+    return 0;
+}
