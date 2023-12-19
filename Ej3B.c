4aa56d347c3ad917509144d3c4f92e449b462b6e
Jiachen <jye@myuax.com>
2023-12-19 02:12:43 +0100

    Mejora para leer frases

--- Ej3B.c
+++ Ej3B.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    FILE *file = fopen("file.txt", "w");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para escritura.\n");
+        return 1;
+    }
+
+    fprintf(file, "Hola, mundo!\n");
+
+    fclose(file);
+
+    file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para lectura.\n");
+        return 1;
+    }
+
+    char buffer[50];
+    if (fscanf(file, "%49[^\n]", buffer) != 1) {
+        printf("Error al leer del archivo.\n");
+        fclose(file);
+        return 1;
+    }
+
+    printf("Leido del archivo: %s\n", buffer);
+    fclose(file);
+
+    return 0;
+}
 
